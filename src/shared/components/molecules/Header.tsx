a2fcd8256b630baa57b08210825440d74f8adbaf
yeooonn <rladuswjd0303@naver.com>
2025-08-17 20:06:24 +0900

    Feat: Header 에 검색창 추가

--- src/shared/components/molecules/Header.tsx
+++ src/shared/components/molecules/Header.tsx
@@ -1,21 +1,23 @@
 import Button from "../atoms/Button";
+import Input from "../atoms/Input";
 import Typography from "../atoms/Typography";
 
 export const Header = () => {
   return (
-    <div className="w-full h-19 border-b border-gray-400 px-4 fixed">
+    <div className="w-full h-19 border-b border-gray-200 px-8 fixed">
       <div className="w-full h-full flex justify-between">
         <div className="flex h-full gap-2 items-center">
           <i className="bi bi-link text-5xl text-blue-500" />
           <Typography.H1 className="font-bold">LinkKeeper</Typography.H1>
+          <Input placeholder="링크 검색" icon="search" />
         </div>
         <div className="flex h-full items-center gap-3">
           <Button.Blue>
-            <i className="bi bi-plus text-2xl" />
+            <i className="bi bi-plus text-xl p-o m-0" />
             <Typography.P1 className="text-white">링크 추가</Typography.P1>
           </Button.Blue>
           <Button.Gray>
-            <i className="bi bi-moon text-2xl" />
+            <i className="bi bi-moon text-xl" />
           </Button.Gray>
         </div>
       </div>
