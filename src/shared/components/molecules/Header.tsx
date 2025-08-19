abd4c88ad6fe5ea4bfa59e4ec2d9791c25eff9e9
yeooonn <rladuswjd0303@naver.com>
2025-08-19 23:58:18 +0900

    Design: border 컬러 변경

--- src/shared/components/molecules/Header.tsx
+++ src/shared/components/molecules/Header.tsx
@@ -1,10 +1,10 @@
-import Button from "../atoms/Button";
-import Input from "../atoms/Input";
-import Typography from "../atoms/Typography";
+import Button from "@/shared/components/atoms/Button";
+import Input from "@/shared/components/atoms/Input";
+import Typography from "@/shared/components/atoms/Typography";
 
 export const Header = () => {
   return (
-    <div className="w-full h-19 border-b border-gray-200 px-8 fixed dark:bg-gray-800 dark:border-b-gray-600 bg-white">
+    <div className="w-full h-19 border-b border-gray-300 px-8 fixed dark:bg-gray-800 dark:border-b-gray-600 bg-white">
       <div className="w-full h-full flex justify-between">
         <div className="flex h-full gap-2 items-center">
           <i className="bi bi-link text-5xl text-blue-500" />
