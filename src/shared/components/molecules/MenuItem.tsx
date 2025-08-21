f4503fb2bc6ffe8ba923f898504f9eb064b8b218
yeooonn <rladuswjd0303@naver.com>
2025-08-21 23:53:11 +0900

    Feat:  FolderItem, MenuItem shard 계층으로 이동

---
+++ src/shared/components/molecules/MenuItem.tsx
@@ -0,0 +1,34 @@
+import cn from "@/shared/utils/cn";
+import Typography from "@/shared/components/atoms/Typography";
+import { MenuInterface } from "@/entites/menu/types";
+
+interface MenuItemProps {
+  menu: MenuInterface;
+  isSelected: boolean;
+  onClick: () => void;
+  selectedColor?: string;
+  unSelectedColor?: string;
+}
+
+export const MenuItem = ({
+  menu,
+  onClick,
+  isSelected,
+  selectedColor,
+  unSelectedColor,
+}: MenuItemProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        isSelected ? selectedColor : unSelectedColor,
+        "w-full flex gap-3 px-3 py-2 mb-2 rounded-lg cursor-pointer transition-all"
+      )}
+    >
+      <i className={menu.icon} />
+      <Typography.P1 className={cn(isSelected ? "text-white" : "")}>
+        {menu.text}
+      </Typography.P1>
+    </button>
+  );
+};
