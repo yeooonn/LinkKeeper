45620d9876683c879bf363ca7f3ea81863c2fe11
yeooonn <rladuswjd0303@naver.com>
2025-08-23 19:54:44 +0900

    Feat: 폴더 더미 데이터 구조 변경

--- src/entites/folder/types.ts
+++ src/entites/folder/types.ts
@@ -2,5 +2,5 @@
 export interface FolderInterface {
   id: string;
   name: string;
-  children?: { id: string; name: string }[];
+  children?: { id: string; name: string; children?:FolderInterface[] }[];
 }
--- src/features/navigation/lib/folderList.ts
+++ src/features/navigation/lib/folderList.ts
@@ -15,7 +15,7 @@ export const FOLDER_LIST: FolderInterface[] = [
   {
     id: "2",
     name: "개인",
-    children: [{ id: "2-1", name: "학습" }],
+    children: [{ id: "2-1", name: "학습", children: [{ id: "2-1-1", name: "타입스크립트" }, { id: "2-1-2", name: "자바스크립트" }] }],
   },
   { id: "3", name: "아카이브" },
 ];
