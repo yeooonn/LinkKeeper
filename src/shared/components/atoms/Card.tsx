388ff785830e99d20d2fbc15a9c9b16934beef45
yeooonn <rladuswjd0303@naver.com>
2025-08-23 23:19:45 +0900

    feat(Card): 이미지가 포함된 ImageCard UI 추가

--- src/shared/components/atoms/Card.tsx
+++ src/shared/components/atoms/Card.tsx
@@ -4,10 +4,9 @@ import { HTMLAttributes, ReactNode } from "react";
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
   className?: string;
   children: ReactNode;
-  darkMode?: boolean;
 }
 
-const Card = ({ className, children, darkMode, ...props }: CardProps) => {
+const Card = ({ className, children, ...props }: CardProps) => {
   return (
     <div
       className={cn(
@@ -23,6 +22,23 @@ const Card = ({ className, children, darkMode, ...props }: CardProps) => {
   );
 };
 
+const ImageCard = ({ className, children, ...props }: CardProps) => {
+  return (
+    <div
+      className={cn(
+        className,
+        "p-4 rounded-xl border",
+        "bg-white border-gray-200 dark:bg-gray-800 dark:border-gray-700",
+        "hover:shadow-md transition-shadow flex gap-5"
+      )}
+      {...props}
+    >
+      <div className="w-25 h-25 bg-gray-500 rounded-sm"></div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
 const Header = ({ className, children, ...props }: CardProps) => {
   return (
     <div className={cn("flex justify-between", className)} {...props}>
@@ -33,7 +49,7 @@ const Header = ({ className, children, ...props }: CardProps) => {
 
 const Content = ({ className, children, ...props }: CardProps) => {
   return (
-    <div className={className} {...props}>
+    <div className={cn("pt-2 pb-3", className)} {...props}>
       {children}
     </div>
   );
@@ -41,13 +57,14 @@ const Content = ({ className, children, ...props }: CardProps) => {
 
 const Footer = ({ className, children, ...props }: CardProps) => {
   return (
-    <div className={cn("flex gap-3", className)} {...props}>
+    <div className={cn("flex gap-3 items-center", className)} {...props}>
       {children}
     </div>
   );
 };
 
 Card.Header = Header;
+Card.ImageCard = ImageCard;
 Card.Content = Content;
 Card.Footer = Footer;
 
