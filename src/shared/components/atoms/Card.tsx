4a494248ca94545c73a8c9ce31d789f8eca81f1c
yeooonn <rladuswjd0303@naver.com>
2025-08-23 23:30:57 +0900

    Desgin: 다크모드 hover 컬러 수정

--- src/shared/components/atoms/Card.tsx
+++ src/shared/components/atoms/Card.tsx
@@ -11,7 +11,7 @@ const Card = ({ className, children, ...props }: CardProps) => {
     <div
       className={cn(
         className,
-        "p-4 rounded-xl border bg-white cursor-pointer hover:border-gray-300 border-gray-200 dark:bg-gray-800 dark:border-gray-700"
+        "p-4 rounded-xl border bg-white cursor-pointer hover:border-gray-300 border-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:border-gray-600"
       )}
       {...props}
     >
@@ -25,7 +25,7 @@ const ImageCard = ({ className, children, ...props }: CardProps) => {
     <div
       className={cn(
         className,
-        "p-4 rounded-xl border bg-white cursor-pointer hover:border-gray-300 border-gray-200 dark:bg-gray-800 dark:border-gray-700 flex gap-5"
+        "p-4 rounded-xl border bg-white cursor-pointer hover:border-gray-300 border-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:border-gray-600 flex gap-5"
       )}
       {...props}
     >
