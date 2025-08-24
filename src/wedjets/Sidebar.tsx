ea0b23dbaaac517fed85b84bac50d30dff16a4ef
yeooonn <rladuswjd0303@naver.com>
2025-08-24 15:54:21 +0900

    Design(Sidebar): 스크롤 안 보이게 설정, 컴포넌트간 간격 수정

--- src/wedjets/Sidebar.tsx
+++ src/wedjets/Sidebar.tsx
@@ -4,11 +4,14 @@ import ProfileSection from "@/features/profile/ui/ProfileSection";
 
 const Sidebar = () => {
   return (
-    <aside className="w-80 dark:bg-gray-800 dark:border-gray-700 bg-white border-gray-300 border-r h-screen overflow-y-auto">
-      <div className="px-4 py-10 mt-30 mb-10 border-b border-gray-200 dark:border-gray-600">
+    <aside
+      style={{ scrollbarWidth: "none" }}
+      className="w-80 h-screen overflow-y-auto dark:bg-gray-800 dark:border-gray-700 bg-white border-gray-300 border-r"
+    >
+      <div className="px-4 pt-25 pb-5 border-b border-gray-200 dark:border-gray-600">
         <ProfileSection />
       </div>
-      <div className="border-b border-gray-200 dark:border-gray-600">
+      <div className="pt-7 border-b border-gray-200 dark:border-gray-600">
         <MenuSection />
       </div>
       <div className="py-6">
