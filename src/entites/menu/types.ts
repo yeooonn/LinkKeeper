a3ca1427a7d924a2bd5cfc6d83701f3fd3d9a6f0
yeooonn <rladuswjd0303@naver.com>
2025-09-07 02:13:00 +0900

    Feat: 선택된 메뉴 아이콘 추가

--- src/entites/menu/types.ts
+++ src/entites/menu/types.ts
@@ -2,5 +2,6 @@
 export interface MenuInterface {
   id: string;
   icon: string;
+  selectedIcon?: string;
   text: string;
 }
--- src/features/navigation/lib/menuList.ts
+++ src/features/navigation/lib/menuList.ts
@@ -2,8 +2,8 @@ import { MenuInterface } from "@/entites/menu/types";
 
 // menu 데이터
 export const MENU_LIST: MenuInterface[] = [
-  { icon: "bi bi-asterisk", text: "전체", id: "전체" },
-  { icon: "bi bi-eye-slash", text: "읽지 않음", id: "읽지 않음" },
-  { icon: "bi bi-star", text: "즐겨찾기", id: "즐겨찾기" },
-  { icon: "bi bi-bell", text: "알림 설정", id: "알림 설정" },
+  { icon: "bi bi-asterisk", selectedIcon: "bi bi-asterisk", text: "전체", id: "전체" },
+  { icon: "bi bi-eye-slash", selectedIcon: "bi bi-eye-slash-fill", text: "읽지 않음", id: "읽지 않음" },
+  { icon: "bi bi-star", selectedIcon: "bi bi-star-fill", text: "즐겨찾기", id: "즐겨찾기" },
+  { icon: "bi bi-bell", selectedIcon: "bi bi-bell-fill", text: "알림 설정", id: "알림 설정" },
 ];
