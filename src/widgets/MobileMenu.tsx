9fdc11353183b785c0c2ed4efb00f238c6792917
yeooonn <rladuswjd0303@naver.com>
2025-09-09 00:03:37 +0900

    Refactor: 반복되던 버튼 렌더링 UI를 재사용 가능한 랜더함수로 추출

--- src/widgets/MobileMenu.tsx
+++ src/widgets/MobileMenu.tsx
@@ -6,6 +6,46 @@ import cn from "@/shared/utils/cn";
 import ProfileSection from "@/features/profile/ui/ProfileSection";
 import FolderSection from "@/features/navigation/ui/FolderSection";
 
+interface RenderMobileMenuButtonType {
+  icon: string;
+  selectedIcon?: string;
+  buttonText: string;
+  isSelected: boolean;
+  onclick: () => void;
+}
+
+const RenderMobileMenuButton = ({
+  icon,
+  selectedIcon,
+  buttonText,
+  isSelected,
+  onclick,
+}: RenderMobileMenuButtonType) => {
+  return (
+    <button className="w-15 h-15 flex-col items-center flex" onClick={onclick}>
+      <i
+        className={cn(
+          "text-xl",
+          isSelected ? selectedIcon || icon : icon,
+          isSelected
+            ? "text-black dark:text-white"
+            : "text-gray-500 dark:text-gray-300"
+        )}
+      />
+      <Typography.P1
+        className={cn(
+          isSelected
+            ? "text-black dark:text-white"
+            : "text-gray-500 dark:text-gray-300",
+          "text-sm!"
+        )}
+      >
+        {buttonText}
+      </Typography.P1>
+    </button>
+  );
+};
+
 const MobileMenu = () => {
   const { selectedMenu, setSelectedMenu } = useNavigation();
   const isSelected = selectedMenu === "카테고리";
@@ -28,62 +68,24 @@ const MobileMenu = () => {
 
       <div className="dark:bg-[#121826] fixed border-t border-gray-200 dark:border-gray-500 bottom-0 w-full h-20 bg-white mobile:inline tablet:hidden laptop:hidden desktop:hidden z-999">
         <div className="flex py-2 px-6 justify-between">
-          <button
-            className="w-15 h-15 flex-col items-center flex"
-            onClick={() => {
-              setSelectedMenu("카테고리");
-            }}
-          >
-            <i
-              className={cn(
-                "bi bi-list text-xl",
-                isSelected
-                  ? "text-black dark:text-white"
-                  : "text-gray-500 dark:text-gray-300"
-              )}
-            />
-            <Typography.P1
-              className={cn(
-                isSelected
-                  ? "text-black dark:text-white"
-                  : "text-gray-500 dark:text-gray-300",
-                "text-sm!"
-              )}
-            >
-              카테고리
-            </Typography.P1>
-          </button>
+          <RenderMobileMenuButton
+            icon="bi bi-list"
+            buttonText="카테고리"
+            isSelected={isSelected}
+            onclick={() => setSelectedMenu("카테고리")}
+          />
 
           {MENU_LIST.map((menu) => {
             const isSelected = selectedMenu === menu.text;
             return (
-              <button
-                className="w-15 h-15 flex-col items-center flex"
-                key={menu.id}
-                onClick={() => {
-                  setSelectedMenu(menu.text);
-                }}
-              >
-                <i
-                  className={cn(
-                    "text-xl",
-                    isSelected ? menu.selectedIcon : menu.icon,
-                    isSelected
-                      ? "text-black dark:text-white"
-                      : "text-gray-500 dark:text-gray-300"
-                  )}
-                />
-                <Typography.P1
-                  className={cn(
-                    "text-sm!",
-                    isSelected
-                      ? "text-black dark:text-white"
-                      : "text-gray-500 dark:text-gray-300"
-                  )}
-                >
-                  {menu.text}
-                </Typography.P1>
-              </button>
+              <RenderMobileMenuButton
+                key={menu.text}
+                icon={menu.icon}
+                selectedIcon={menu.selectedIcon}
+                buttonText={menu.text}
+                isSelected={isSelected}
+                onclick={() => setSelectedMenu(menu.text)}
+              />
             );
           })}
         </div>
