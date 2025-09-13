c0e2480bdb5708af8c228ed13a67d18d03470fe2
yeooonn <rladuswjd0303@naver.com>
2025-09-14 01:04:12 +0900

    Feat: 새 링크 추가 모달(AddLinkModal)에 폴더 선택 기능 추가
    - 기존 폴더 선택 혹은 새 폴더를 바로 추가할 수 있도록 개발

--- src/features/add-link/ui/AddLinkModal.tsx
+++ src/features/add-link/ui/AddLinkModal.tsx
@@ -1,16 +1,118 @@
+import { useNavigation } from "@/features/navigation/model/useNavigation";
+import FolderSection from "@/features/navigation/ui/FolderSection";
+import Button from "@/shared/components/atoms/Button";
+import LineStepper from "@/shared/components/atoms/LineStepper";
 import Modal from "@/shared/components/atoms/Modal";
+import Typography from "@/shared/components/atoms/Typography";
+import LabeledInput from "@/shared/components/molecules/LabeledInput";
+import LabeledTextarea from "@/shared/components/molecules/LabeledTextarea";
+import useLineStepper from "@/shared/hooks/useLineStepper";
+import cn from "@/shared/utils/cn";
+import { useEffect, useState } from "react";
 
 interface AddLinkModalProps {
   closeModal: () => void;
 }
 
 const AddLinkModal = ({ closeModal }: AddLinkModalProps) => {
+  const lineCount = 3;
+  const {
+    step,
+    showNextButton,
+    showPrevButton,
+    showAddButton,
+    handleClickNextButton,
+    handleClickPrevButton,
+  } = useLineStepper({ lineCount: lineCount });
+  const { selectedMenu, setSelectedMenu } = useNavigation();
+  const [showAddFolderInput, setShowAddFolderInput] = useState<boolean>(false);
+
+  // useEffect(() => {
+  //   if (selectedMenu) {
+  //     setShowAddFolderInput(false);
+  //   } else {
+  //     setShowAddFolderInput(true);
+  //   }
+  // }, [selectedMenu]);
+
+  const handleClickNewFolderButton = () => {
+    setShowAddFolderInput((prev) => !prev);
+    setSelectedMenu("");
+  };
+
   return (
-    <Modal onClose={closeModal}>
-      <Modal.Header onClose={closeModal}>링크 추가</Modal.Header>
-      <Modal.Content>컨텐츠</Modal.Content>
-      <Modal.Footer>푸터</Modal.Footer>
-    </Modal>
+    <>
+      <Modal onClose={closeModal}>
+        <Modal.Header onClose={closeModal}>
+          <Typography.P2 className="font-bold">새 링크 추가</Typography.P2>
+        </Modal.Header>
+        <Modal.Content>
+          <LineStepper lineCount={lineCount} step={step} />
+          {step === 0 && (
+            <>
+              <LabeledInput title="제목" placeholder="제목" />
+              <LabeledInput title="URL" placeholder="http://example.com" />
+              <LabeledInput title="태그" placeholder="#태그1  #태그2" />
+              <LabeledTextarea title="메모" placeholder="메모" />
+            </>
+          )}
+          {step === 1 && (
+            <div>
+              <div className="flex justify-between mb-3 items-center">
+                <Typography.P1>폴더 선택</Typography.P1>
+                <Button.OutlineBlue onClick={handleClickNewFolderButton}>
+                  {showAddFolderInput ? "취소" : "폴더 추가"}
+                </Button.OutlineBlue>
+              </div>
+
+              <div className="border border-gray-300 rounded-lg pt-2 ">
+                {showAddFolderInput && (
+                  <div
+                    onClick={() => {
+                      // if (!showFolderSelectionHighlight) onClick();
+                      // setSelectedMenu(folder.name);
+                    }}
+                    className={cn(
+                      // isSelected ? selectedColor : unSelectedColor,
+                      // !isSelected &&
+                      "group pl-2.5 flex gap-3 ml-3 mt-4 mr-3 pr-2 rounded-lg cursor-pointer  items-center dark:hover:bg-[#4d6080] hover:bg-gray-100"
+                    )}
+                  >
+                    <div className="flex gap-2">
+                      <i className="bi bi-chevron-right desktop:text-base text-xs" />
+                      <i className="bi bi-folder desktop:text-base text-xs" />
+                    </div>
+                    <LabeledInput
+                      placeholder="폴더명 입력"
+                      className="h-8"
+                      inputClassName="h-8 border border-gray-300 bg-transparent mt-1.5 !group-focus-within:border-transparent"
+                    />
+                  </div>
+                )}
+                <FolderSection
+                  showTitle={false}
+                  showFolderSelectionHighlight={true}
+                  showChildFolderSelectionHighlight={false}
+                  selectedMenu={selectedMenu}
+                  setSelectedMenu={setSelectedMenu}
+                />
+              </div>
+            </div>
+          )}
+          {step === 2 && <p>알림설정</p>}
+        </Modal.Content>
+        <Modal.Footer>
+          <Button.Gray onClick={closeModal}>취소</Button.Gray>
+          {showPrevButton && (
+            <Button.Gray onClick={handleClickPrevButton}>이전</Button.Gray>
+          )}
+          {showNextButton && (
+            <Button.Gray onClick={handleClickNextButton}>다음</Button.Gray>
+          )}
+          {showAddButton && <Button.Blue>추가</Button.Blue>}
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
