900a9cf1ad43e37fe75050c50536343541adf581
yeooonn <rladuswjd0303@naver.com>
2025-09-22 00:05:12 +0900

    Feat: 버튼 컴포넌트에 isDisabled 속성 추가 및 상태에 따른 css 개선

--- src/shared/components/atoms/Button.tsx
+++ src/shared/components/atoms/Button.tsx
@@ -7,29 +7,42 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   children?: ReactNode;
   color?: ButtonColor;
+  isDisabled?: boolean;
 }
 
 const ButtonComponent = ({
   className,
   children,
   color = "Gray",
+  isDisabled = false,
   ...props
 }: ButtonProps) => {
   const ButtonColor: Record<ButtonColor, string> = {
-    Gray: "bg-button-gray-bg hover:bg-button-gray-hover-bg text-button-gray-text border-button-gray-border",
-    Blue: "bg-button-blue-bg hover:button-blue-hover-bg text-button-blue-text border-button-blue-border",
-    OutlineBlue:
-      "text-button-outline-blue-text hover:text-button-outline-blue-hover-text border border-button-outline-blue-border hover:border-button-outline-blue-hover-border",
+    Gray: cn(
+      "bg-button-gray-bg text-button-gray-text border-button-gray-border",
+      !isDisabled && "hover:bg-button-gray-hover-bg"
+    ),
+    Blue: cn(
+      "bg-button-blue-bg text-button-blue-text border-button-blue-border",
+      !isDisabled && "hover:button-blue-hover-bg"
+    ),
+    OutlineBlue: cn(
+      "text-button-outline-blue-text border border-button-outline-blue-border",
+      !isDisabled &&
+        "hover:text-button-outline-blue-hover-text hover:border-button-outline-blue-hover-border"
+    ),
   };
 
   return (
     <button
       className={cn(
-        "desktop:px-4 desktop:py-2 laptop:px-3 laptop:py-1 px-3 py-1 tablet:rounded-lg mobile:rounded-sm flex items-center space-x-1 cursor-pointer transition-all",
+        "desktop:px-4 desktop:py-2 laptop:px-3 laptop:py-1 px-3 py-1 tablet:rounded-lg mobile:rounded-sm flex items-center space-x-1 transition-all",
         ButtonColor[color],
+        isDisabled ? "cursor-default" : "cursor-pointer",
         className
       )}
       {...props}
+      disabled={isDisabled}
     >
       {children}
     </button>
