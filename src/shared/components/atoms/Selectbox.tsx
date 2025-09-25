b21f4688559479e98af883f5a8e625de20ba63e5
yeooonn <rladuswjd0303@naver.com>
2025-09-25 23:55:39 +0900

    Feat: Selectbox options 타입 수정

--- src/shared/components/atoms/Selectbox.tsx
+++ src/shared/components/atoms/Selectbox.tsx
@@ -1,25 +1,29 @@
-import { FolderInterface } from "@/entites/folder/types";
 import cn from "@/shared/utils/cn";
 import { SelectHTMLAttributes } from "react";
 
 interface SelectBoxProps extends SelectHTMLAttributes<HTMLSelectElement> {
   className?: string;
-  options: FolderInterface[];
+  options: { key: string; value: string }[];
 }
 
 const Selectbox = ({ className, options, ...props }: SelectBoxProps) => {
   return (
     <div className="relative w-full">
       <select
+        defaultValue=""
         className={cn(
           "px-2 py-2 w-full  rounded-lg border bg-white border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm",
           className
         )}
         {...props}
       >
-        <option disabled>선택해 주세요.</option>
+        <option value="" disabled>
+          선택해 주세요.
+        </option>
         {options.map((option) => (
-          <option key={option.id}>{option.name}</option>
+          <option key={option.key} value={option.value}>
+            {option.value}
+          </option>
         ))}
       </select>
     </div>
