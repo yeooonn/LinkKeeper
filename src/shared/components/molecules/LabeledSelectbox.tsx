ac73a6f0f53bba500410c7734f37d4a952ae4d1a
yeooonn <rladuswjd0303@naver.com>
2025-09-25 23:54:55 +0900

    Feat: LabeledSelectbox 컴포넌트 react-hook-form 연결

--- src/shared/components/molecules/LabeledSelectbox.tsx
+++ src/shared/components/molecules/LabeledSelectbox.tsx
@@ -1,25 +1,35 @@
 import cn from "@/shared/utils/cn";
 import Typography from "../atoms/Typography";
-import { FolderInterface } from "@/entites/folder/types";
 import { SelectHTMLAttributes } from "react";
 import Selectbox from "@/shared/components/atoms/Selectbox";
+import { FieldError, UseFormRegisterReturn } from "react-hook-form";
 
 interface LabeledSelectboxProps
   extends SelectHTMLAttributes<HTMLSelectElement> {
   title: string;
   className?: string;
-  options: FolderInterface[];
+  options: { key: string; value: string }[];
+  error?: FieldError | string; // 에러 메시지 또는 객체
+  register?: UseFormRegisterReturn; // react-hook-form의 register 반환값
 }
 
 const LabeledSelectbox = ({
   title,
   className,
   options,
+  error,
+  register,
+  ...props
 }: LabeledSelectboxProps) => {
   return (
     <div className={cn("mb-3", className)}>
       <Typography.P1 className="mb-1">{title}</Typography.P1>
-      <Selectbox options={options} />
+      <Selectbox options={options} {...register} {...props} />
+      {error && (
+        <Typography.P3 className="text-[tomato] mt-1">
+          {typeof error === "string" ? error : error.message}
+        </Typography.P3>
+      )}
     </div>
   );
 };
