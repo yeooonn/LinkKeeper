085c751a4882bdcfb944fc3b454ea82a7d10d3bd
yeooonn <rladuswjd0303@naver.com>
2025-09-30 00:26:18 +0900

    Feat: 불필요한 주석 제거

--- src/shared/lib/db.tsx
+++ src/shared/lib/db.tsx
@@ -1,25 +1,6 @@
+// src/shared/lib/db.ts
 import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
-
-// async function test() {
-//   const user = await db.user.create({
-//     data: {
-//       userName: "yeooonn",
-//     },
-//   });
-
-//   // const user = await db.user.findMany({
-//   //   where: {
-//   //     userName: {
-//   //       contains: "yeo",
-//   //     },
-//   //   },
-//   // });
-
-//   console.log(user);
-// }
-
-// test();
 
 export default db;
