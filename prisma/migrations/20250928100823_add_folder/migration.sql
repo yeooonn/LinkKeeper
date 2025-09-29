0102deeb688b4e70e9d3632acaa3351aa762843f
yeooonn <rladuswjd0303@naver.com>
2025-09-29 18:58:49 +0900

    Feat: supabase 초기 설정 및 prisma 연동

--- prisma/migrations/20250928100823_add_folder/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Folder" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "parentId" TEXT,
-    CONSTRAINT "Folder_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Folder" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
