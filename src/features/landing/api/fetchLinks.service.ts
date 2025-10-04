3491a2c3e96050efc1b48e353e3dcdf44eb251cb
yeooonn <rladuswjd0303@naver.com>
2025-10-05 03:23:33 +0900

    Feat(fetchLinks): method 추가

--- src/features/landing/api/fetchLinks.service.ts
+++ src/features/landing/api/fetchLinks.service.ts
@@ -2,9 +2,10 @@ import { LinkResponse } from "@/features/landing/model/link.type";
 import { apiClient } from "@/shared/utils/apiClient";
 
 // 서버 컴포넌트에서 API 호출로 데이터 가져오기
-async function fetchLinks(): Promise<LinkResponse[]> {
+async function fetchLinks(revalidateTime: number = 10): Promise<LinkResponse[]> {
   const res = await apiClient<LinkResponse[]>("/api/links", {
-    revalidate: 10, // 10초마다 캐시 재검증
+    method: "GET",
+    revalidate: revalidateTime, // 10초마다 캐시 재검증
   });
 
   return res ?? [];
