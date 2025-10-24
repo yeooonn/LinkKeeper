ce7aed871157b7439332d3f51dff4cdb6b1339ca
yeooonn <rladuswjd0303@naver.com>
2025-10-24 18:08:51 +0900

    Feat: 유저 정보 저장 방식 수정

--- src/app/api/auth/sync-user/route.ts
+++ src/app/api/auth/sync-user/route.ts
@@ -4,25 +4,26 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   try {
     const body = await request.json();
+    const { id, email, name, profileImage } = body;
 
-    if (!body.id) return NextResponse.json("유저 데이터가 없습니다.");
+    if (!id) return NextResponse.json("유저 데이터가 없습니다.");
 
-    const existing = await db.user.findUnique({
-      where: { id: body.id },
+    const user = await db.user.upsert({
+      where: { id: id },
+      update: {
+        email: email,
+        name: name,
+        profileImage: profileImage,
+      },
+      create: {
+        id: id,
+        email: email,
+        name: name,
+        profileImage: profileImage,
+      },
     });
 
-    if (!existing) {
-      await db.user.create({
-        data: {
-          id: body.id,
-          email: body.email,
-          name: body.name,
-          profileImage: body.profileImage || null,
-        },
-      });
-    }
-
-    return NextResponse.json({ message: "success" });
+    return NextResponse.json({ message: "success", data: user });
   } catch (error) {
     console.error("Error creating link:", error);
     return NextResponse.json({ error: "로그인 실패" }, { status: 500 });
