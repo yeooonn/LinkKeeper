97a1aeeed6b8daed20a2caf64daa26e31dd74d37
yeooonn <rladuswjd0303@naver.com>
2025-10-27 02:29:26 +0900

    Refactor(auth): 서버에서 토큰 재검증 위해 getSession -> getUser 로 변경

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 // pages/auth/callback.tsx
 import { useEffect } from "react";
-import { createClient } from "@/shared/utils/supabase/supabaseClient";
+import { createClient } from "@/shared/utils/supabase/client";
 import { useRouter } from "next/navigation";
-import { fetchAPI } from "@/shared/utils/fetchAPI";
 import { SignIn } from "@/features/sign-in/model/signIn.service";
 import { toast } from "react-toastify";
 import { useAuthStore } from "@/shared/stores/useUserStore";
@@ -15,18 +14,18 @@ export default function AuthCallback() {
 
   useEffect(() => {
     const handleAuth = async () => {
-      const { data, error } = await supabase.auth.getSession();
+      const { data, error } = await supabase.auth.getUser();
       if (error) {
         console.error(error);
         return;
       }
 
-      if (data?.session) {
+      if (data?.user) {
         const requestData = {
-          id: data.session.user.id,
-          email: data.session.user.email,
-          name: data.session.user.user_metadata.name,
-          profileImage: data.session.user.user_metadata.avatar_url || null,
+          id: data.user.id,
+          email: data.user.email,
+          name: data.user.user_metadata.name,
+          profileImage: data.user.user_metadata.avatar_url || null,
         };
 
         const response = await SignIn(requestData);
