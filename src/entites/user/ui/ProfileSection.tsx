ab530aa99dac33d8e0fb2329a0be85c02dac630f
yeooonn <rladuswjd0303@naver.com>
2025-10-27 20:39:54 +0900

    Feat: 로그아웃 기능 추가

--- src/entites/user/ui/ProfileSection.tsx
+++ src/entites/user/ui/ProfileSection.tsx
@@ -3,9 +3,20 @@ import Typography from "@/shared/components/atoms/Typography";
 import SignInButton from "@/features/sign-in/ui/SignInButton";
 import { UserInterface } from "@/entites/user/model/types";
 import { useUser } from "@/shared/hooks/useUser";
+import { useState } from "react";
+import { createClient } from "@/shared/utils/supabase/client";
 
 const ProfileContent = ({ userData }: { userData: UserInterface }) => {
+  const supabase = createClient();
   const { profileImage, name } = userData;
+  const [isOpenArrow, setIsOpenArrow] = useState<boolean>(false);
+
+  // 로그아웃
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    window.location.reload();
+  };
+
   return (
     <div className="flex gap-4 items-center mobile:my-3 tablet:m-0 desktop:mt-0">
       <Profile
@@ -16,9 +27,29 @@ const ProfileContent = ({ userData }: { userData: UserInterface }) => {
         <Typography.P1 className="desktop:text-xl text-base font-bold">
           {name}
         </Typography.P1>
-        <div className="flex justify-between cursor-pointer">
-          <Typography.P1>계정관리</Typography.P1>
-          <i className="bi bi-chevron-right" />
+        <div className="w-full flex justify-between cursor-pointer relative">
+          <div
+            onClick={() => setIsOpenArrow((prev) => !prev)}
+            className="w-full flex justify-between"
+          >
+            <Typography.P1>계정관리</Typography.P1>
+            {!isOpenArrow && <i className="bi bi-chevron-right" />}
+          </div>
+          {isOpenArrow && (
+            <>
+              <i
+                className="bi bi-chevron-down"
+                onClick={() => setIsOpenArrow(false)}
+              />
+              <button
+                className="px-4 py-2 mt-7 bg-background-secondary  border border-border-secondary absolute rounded-xl right-0 shadow whitespace-nowrap cursor-pointer hover:bg-background-hover transition-all"
+                onClick={() => handleLogout()}
+              >
+                <i className="bi bi-door-closed mr-2" />
+                로그아웃
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
